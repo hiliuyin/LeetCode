e749802d1847453794a929c65b9e5f4570145175
lvlili <lvlili555@163.com>
2016-05-30 16:26:29 +0800

    Create 104.MaximumDepthOfBinaryTree.cpp

---
+++ 104.MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,27 @@
+/* Question
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+*/
+
+/* 思路：
+递归求解左右子树的最大深度
+/*
+
+// Code:
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root == nullptr) return 0;
+        return std::max(maxDepth(root->left), maxDepth(root->right)) + 1;
+    }
+};
