336ee03512e6b28a02cd13faef018e47cc6beb4b
lvlili <lvlili555@163.com>
2016-06-03 11:18:15 +0800

    Create 24.SwapNodesInPairs.cpp

---
+++ Important/24.SwapNodesInPairs.cpp
@@ -0,0 +1,63 @@
+/*Question:
+Given a linked list, swap every two adjacent nodes and return its head.
+
+For example,
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+
+Subscribe to see which companies asked this question
+
+Tags: Lined List
+*/
+
+/*思路：
+连续处理链表的两个元素，注意考虑链表有偶数个元素和奇数个元素两种情况
+*/
+
+//Code:
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(head==nullptr || head->next==nullptr)
+            return head;
+            
+        ListNode* resultHead = head->next;
+        ListNode* pre = head;
+        ListNode* cur1 = head;
+        ListNode* cur2 = cur1->next;
+        
+        cur1->next = cur2->next;
+        cur2->next = cur1;
+        cur1 = cur1->next;
+        
+        while(cur1!=nullptr)
+        {
+            cur2 = cur1->next;
+            
+            if(cur2!=nullptr)  //链表有偶数元素
+            {
+                cur1->next = cur2->next;
+                cur2->next = cur1;
+                pre->next = cur2;
+            }
+            else
+            {
+                cur1->next = nullptr;
+                pre->next = cur1;
+            }
+                
+            pre = cur1;    
+            cur1 = cur1->next;
+        }
+        return resultHead;
+    }
+};
