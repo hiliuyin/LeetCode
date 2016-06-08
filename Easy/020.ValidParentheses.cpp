2a5558e63b5960a6aa40f0578bda197fb7de0315
lvlili <lvlili555@163.com>
2016-06-08 09:17:16 +0800

    Create 020.ValidParentheses.cpp

---
+++ Easy/020.ValidParentheses.cpp
@@ -0,0 +1,80 @@
+/*Question:
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+Tags: stack, string
+*/
+
+/*思路:
+如果是( [ {则进栈，否则与栈顶元素比较
+*/
+
+//Code:
+//Code 1:
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.size() <= 1) return false;
+        std::vector<char> check;
+        
+        for(auto item: s)
+        {
+            switch(item)
+            {
+                case '(':
+                    check.push_back(item);
+                    break;
+                case '[':
+                    check.push_back(item);
+                    break;
+                case '{':
+                    check.push_back(item);
+                    break;
+                case ')':
+                    if(check.empty() || check.back() != '(')
+                        return false;
+                    check.pop_back();
+                    break;
+                  case ']':
+                    if(check.empty() || check.back() != '[')
+                        return false;
+                    check.pop_back();
+                    break;
+                  case '}':
+                    if(check.empty() || check.back() != '{')
+                        return false;
+                    check.pop_back();
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        if(check.empty()) return true;
+        return false;
+    }
+};
+
+//Code 2:
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.size() <= 1) return false;
+        std::string check;
+        
+        for(auto item: s)
+        {
+            if(item == ')' && check.back() == '(')
+                check.pop_back();
+            else if(item == ']' && check.back() == '[')
+                check.pop_back();
+            else if(item == '}' && check.back() == '{')
+                check.pop_back();
+            else 
+                check.push_back(item);
+        }
+    
+        return check.empty();
+    }
+};
