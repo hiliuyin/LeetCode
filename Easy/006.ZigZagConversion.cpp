09e93f8a8d46af3a0a834e13c10ffc39dd2fa5cd
lvlili <lvlili555@163.com>
2016-06-19 21:23:06 +0800

    Create 006.ZigZagConversion.cpp

---
+++ Easy/006.ZigZagConversion.cpp
@@ -0,0 +1,48 @@
+/*
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+Write the code that will take a string and make this conversion given a number of rows:
+string convert(string text, int nRows);
+convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+*/
+
+//Code:
+//Code 1:
+    string convert(string s, int numRows) {
+        if(s.empty() || numRows < 2) return s;
+        std::vector<std::string> vec(numRows);
+        
+        int row = 0;
+        bool bUp = false;
+        for(auto&& ch : s)
+        {
+            vec[row] += ch;
+            
+            if(!bUp)
+            {
+                ++row;
+                if(row == numRows)
+                {
+                    row = numRows - 2;
+                    bUp = true;
+                }
+            }
+            else
+            {
+                --row;
+                if(row < 0)
+                {
+                    row = 1;
+                    bUp = false;
+                }
+            }
+        }
+        
+        std::string result;
+        for(int i = 0; i < numRows; ++i)
+            result += vec[i];
+        return result;
+    }
