cf60407f71f31c5d06a545377fafd1660dbc8ad1
lvlili <lvlili555@163.com>
2016-06-19 21:44:31 +0800

    Update 006.ZigZagConversion.cpp

--- Easy/006.ZigZagConversion.cpp
+++ Easy/006.ZigZagConversion.cpp
@@ -46,3 +46,31 @@ convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
             result += vec[i];
         return result;
     }
+//Code 2:
+std::string convert(std::string s, int nRows)
+{
+    if (s.size() <= 1 || nRows <= 1) return s;
+
+    std::string result;
+
+    for (int i = 0, iEnd = s.size(); i < iEnd; i += nRows*2-2)
+        result.append(1, s[i]);
+
+    for (int r = 1; r < nRows-1; ++r)
+    {
+        const int steps = 2 * nRows - 2 * r - 2;
+        for (int i = r, iEnd = s.size(), k = 0; i < iEnd;)
+        {
+            result.append(1, s[i]);
+            if (k++ % 2 == 0)
+                i += steps;
+            else
+                i += r*2;
+        }
+    }
+
+    for (int i = nRows - 1, iEnd = s.size(); i < iEnd; i += nRows * 2 - 2)
+        result.append(1, s[i]);
+
+    return result;
+}
